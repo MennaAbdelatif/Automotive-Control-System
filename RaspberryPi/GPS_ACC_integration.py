72025196ecd51d5eb323c7ac505fde1a68e3112f
MennaAbdelatif <141871133+MennaAbdelatif@users.noreply.github.com>
2025-10-16 03:52:29 +0300

    GPS_ACC_integration.py

--- RaspberryPi/GPS_ACC_integration.py
+++ RaspberryPi/GPS_ACC_integration.py
@@ -8,15 +8,17 @@
 
 import time
 import string
-
-#GPS used libraries
 import serial
+import Adafruit_ADXL345
+import paho.mqtt.client as mqtt
 import string
 import pynmea2
 import sys
 
-# ADXL345 module used libraries
-import Adafruit_ADXL345
+BROKER = "test.mosquitto.org"      
+PORT = 1883
+TOPIC_LIVE = "car/location"
+TOPIC_ALERT = "car/alert"
 
 
 # Create an ADXL345 instance.
@@ -24,34 +26,69 @@ accel = Adafruit_ADXL345.ADXL345()
 
 #Intialization of UART for recieving data from GPS module and send data to stm32 microcontroller
 port="/dev/ttyAMA0"
-ser = serial.Serial(port,baudrate = 9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=0.5)
+ser = serial.Serial(
+    port,
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=0.5
+)
 
+# Initialize MQTT Client
+client = mqtt.Client("CarTracker")
+client.connect(BROKER, PORT, 60)
+client.loop_start()
 
+# Timing for periodic live location
+LIVE_INTERVAL = 15      # seconds between live updates
+last_live_time = 0
+
+# Main Loop
 while True:
+    try:
+        newdata = ser.readline()
+        if sys.version_info[0] == 3:
+            newdata = newdata.decode("utf-8", "ignore")
 
-    #Getting the data from the GPS module and store it in a variable
-    dataout = pynmea2.NMEAStreamReader()
-    newdata=ser.readline()
-    if sys.version_info[0] == 3: 
-        newdata = newdata.decode("utf-8","ignore")
-		
-    # Read the X, Y, Z axis acceleration values and print them.
-    x, y, z = accel.read()
-	
-    #Parsing the output data from GPS to get latitude and longitude
-    if newdata[0:6] == "$GPRMC":
-        newmsg=pynmea2.parse(newdata)
-        lat=newmsg.latitude
-        lng=newmsg.longitude
-		
-		#the string that will sent to the stm32 microcontroller
-		#the first thing in the string is the status of the accelerometer that indicates that accident happened
-		#then the latitude and longitude of the vehicle's location 
-		#the latitude and the longitude are seprated by * and end by # for making parsing easier when string recieved by stm32 microcontroller
-        gps="1"+str(lat)+"*"+ str(lng)+"#"
-		
-		#check on the readings of the Acceleromter to detect the vehicle's behaviour
-        if -200>x>-300 or 300>x>250 or 300>y>200 or -200>y>-300:
-            ser.write(gps.encode())
-            time.sleep(0.5)
-      
+        # Read accelerometer data
+        x, y, z = accel.read()
+
+        # Parse GPS data
+        if newdata.startswith("$GPRMC"):
+            newmsg = pynmea2.parse(newdata)
+            lat = newmsg.latitude
+            lng = newmsg.longitude
+
+            # Accident detection (X/Y spikes)
+            if abs(x) > 250 or abs(y) > 250:
+                alert_msg = f"Accident detected! Latitude: {lat}, Longitude: {lng}"
+                print(alert_msg)
+
+                # Publish accident alert via MQTT
+				
+                client.publish(TOPIC_ALERT, alert_msg)
+
+                # Send formatted GPS string to STM32
+				
+                gps = f"1{lat}*{lng}#"
+                ser.write(gps.encode())
+                print("Sent to STM32",gps)
+                time.sleep(1)
+
+            # Live location update every LIVE_INTERVAL seconds
+			
+            if time.time() - last_live_time >= LIVE_INTERVAL:
+                live_msg = f"Latitude: {lat}, Longitude: {lng}"
+                client.publish(TOPIC_LIVE, live_msg)
+                print("Live MQTT update ", live_msg)
+                last_live_time = time.time()
+
+        time.sleep(0.5)
+
+    except pynmea2.ParseError:
+        continue
+
+    except Exception as e:
+        print( "Error ")
+        time.sleep(1)
